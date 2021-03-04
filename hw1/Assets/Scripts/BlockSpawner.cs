b6d3a0b636511229616c6e2be50860821e3a86d8
gulnihal <k.glnhl@gmail.com>
2021-03-04 02:58:08 +0100

    draggable blocks created

---
+++ hw1/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class BlockSpawner : MonoBehaviour
+{
+    
+    [Space]
+    [SerializeField]
+    private Sprite pikaSprite; // Serializable, for being able to attach the photo from unity
+
+    [Space]
+    [SerializeField]
+    private GameObject PikaCube; // get original object from unity
+                               // and make the clone of it -in line 30 
+
+     [Space]
+     [SerializeField]
+     private float cubeSize = .1f;
+
+     Vector3 cubePos;
+     Texture2D texture2D;
+
+    public Material pikaMaterial;
+
+     void Awake()
+     {
+         texture2D = pikaSprite.texture;
+        
+
+         for(int i=0; i< texture2D.width; i++)
+         {
+             for(int j=0; j< texture2D.height; j++)
+             {
+
+                 Color color = texture2D.GetPixel(i, j);
+                 // color.a : alpha component of the color ( 0- transparent, 1- opaque ) 
+                 if(color.a == 0)
+                 {
+                     continue;
+                 }
+
+                
+                 //cubePos = new Vector3(i*offsetX - texture2D.width*0.5f, 0f, j*offsetY-texture2D.height*.5f); // fazla büyük
+                 //cubePos = new Vector3(i * cubeSize - texture2D.width * 0.5f, 0f, j * cubeSize - texture2D.height * .5f);
+                 // cubeSize ile çarpınca hesapladığı oranlar değişti, image ı sol altta bi yere bastı
+                 cubePos = new Vector3(cubeSize*( i- (texture2D.width * 0.5f)), 0f, cubeSize*(j - (texture2D.height * .5f)));
+                 //GameObject cubeObj = Instantiate(PikaCube, cubePos, Quaternion.identity, transform);
+                 GameObject cubeObj = Instantiate(PikaCube, transform);
+                 cubeObj.transform.localPosition = cubePos;
+                 cubeObj.transform.localScale = cubeSize* Vector3.one;
+                 cubeObj.GetComponent<Renderer>().material.color = pikaMaterial.color;
+
+    
+            }
+        }
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+}
---
+++ hw1/Assets/Scripts/BlocksDrag.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksDrag : MonoBehaviour
+{
+
+    Texture2D texture2D;
+    Vector3 cubePos;
+
+    [Space]
+    [SerializeField]
+    private Sprite pikaSprite;
+
+    [Space]
+    [SerializeField]
+    private float cubeSize = .1f;
+
+    [Space]
+    [SerializeField]
+    private GameObject cube;
+
+    private void Awake()
+    {
+        texture2D = pikaSprite.texture;
+
+
+        for (int i = 0; i < texture2D.width; i++)
+        {
+            for (int j = 0; j < texture2D.height; j++)
+            {
+
+                Color color = texture2D.GetPixel(i, j);
+                if (color.a == 0) continue;
+                cubePos = new Vector3(cubeSize * (i - (texture2D.width * 0.5f)), 0f, cubeSize * (j - (texture2D.height * .5f)));
+                GameObject cubeObj = Instantiate(cube, cubePos, Quaternion.identity , transform);
+                cubeObj.transform.localScale = cubeSize * Vector3.one;
+                cubeObj.transform.localPosition = cubePos;
+                cubeObj.GetComponent<Renderer>().material.color = color;
+
+
+            }
+        }
+
+    }
+}
